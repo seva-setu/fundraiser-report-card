1ad8134671b1b1f4650f4c52650a4382672ccb55
Udit agarwal <uditagarwal1997@gmail.com>
2017-05-05 04:27:10 +0530

    Added favicon + increased responsiveness

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-Setting up git env
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>
+		Progress report
+	</title>
+	<meta name="description" content="Progress report of sevasetu's citizen care subprogram">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		.div-1{
+		height: 30vh;
+		background-color: #ce93d8;
+	}
+		.logo{
+    position: inherit;
+    margin-left: 3.4%;
+    margin-top: 1.2%;
+    border: 3px solid #4a148c;
+	}
+	/* for screens that are at least 320 pixels wide but less than or equal to 640 pixels wide */
+	@media screen and (min-width:320px) and (max-width:640px) and (orientation:portrait) {
+		.div-1{
+			height: 18vh;
+		}
+  
+	}
+	@media screen and (min-width:320px) and (max-width:640px) and (orientation:landscape) {
+		.div-1{
+			height: 40vh;
+		}
+  
+	}
+
+	</style>
+</head>
+<body>
+<div class="div-1">
+	<img class="logo" src="img/logo.jpg" height=100 width=75></img>
+	
+</div>
+</body>
+</html>
