1779769dbb7ffad1ed9031fd21c083aa3527e279
Udit agarwal <uditagarwal1997@gmail.com>
2017-05-06 01:15:24 +0530

    Division 1 complete + Progress bar added

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>
 		Progress report
@@ -9,36 +12,56 @@
 	<meta name="description" content="Progress report of sevasetu's citizen care subprogram">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<style type="text/css">
-		.div-1{
-		height: 30vh;
-		background-color: #ce93d8;
-	}
-		.logo{
-    position: inherit;
-    margin-left: 3.4%;
-    margin-top: 1.2%;
-    border: 3px solid #4a148c;
-	}
+		
 	/* for screens that are at least 320 pixels wide but less than or equal to 640 pixels wide */
 	@media screen and (min-width:320px) and (max-width:640px) and (orientation:portrait) {
-		.div-1{
-			height: 18vh;
-		}
-  
+		
+  		body{font-size: 10px;}
 	}
 	@media screen and (min-width:320px) and (max-width:640px) and (orientation:landscape) {
-		.div-1{
-			height: 40vh;
-		}
-  
+		
+  		body{font-size: 14px;}
+	}
+	h1{
+		font-size: 3em;
+	}
+	h2{
+		font-size: 2em;
 	}
 
 	</style>
 </head>
 <body>
-<div class="div-1">
-	<img class="logo" src="img/logo.jpg" height=100 width=75></img>
+	<div class="container-fluid" style="background-color:#e1bee7">
+		<div class="row">
+			<div class="col-xs-3">
+				<img class="img-responsive" src="img/logo.jpg" height=100 width=75 style="position: relative;left:1%;margin-top:1em;"></img>
+			</div>
+			<div class="col-xs-9" style="text-align:center;">
+				
+				<h1 style="position: relative ;margin-top: 1%;font-family: Andale Mono,AndaleMono,monospace;font-weight: bold;">Seva Setu</h1>
+				
+				<h2 style="position: relative ;font-family: Andale Mono,AndaleMono,monospace;">Progress Report: Citizen Care</h2>
+				<h2 style="position: relative ;font-family: Andale Mono,AndaleMono,monospace;">April - June 2017</h2>
+			</div>
+		</div>
+	</div>
 	
-</div>
+		<div class="row">
+			<div class="col-sm-7" style="background-color:#ce93d8">
+				<h1>Title Of Project</h1>
+				<div class="progress">
+    				<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%;background-color:#7b1fa2">
+     				 70% Achieved
+    				</div>
+  				</div>
+			</div>
+			<div class="col-sm-5" style="background-color:#ba68c8">
+				<h1>Area of work</h1>
+				<br>
+				<br>
+			</div>
+		</div>
+	
 </body>
 </html>
